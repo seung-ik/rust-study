7a97c58e446a07f6a40cdfc84b57b60ba274fa73
seung-ik <69374857+seung-ik@users.noreply.github.com>
2023-01-27 22:44:24 +0900

    chapter4 slice

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    let five_hundred = tup.0;
-	println!("{five_hundred}");
-    println!("The value of y is: {y}");
-}
+    let s = String::from("hello");
+
+    let slice = &s[0..s.len()];
+    println!("{}",slice)
+
+
+} 
